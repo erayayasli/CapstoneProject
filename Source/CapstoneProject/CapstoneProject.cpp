59ccec140d40c548a803c8f860d89f336c21f4ee
erayayasli <erayayasli623@gmail.com>
2024-10-28 02:06:48 +0300

    Init

---
+++ Source/CapstoneProject/CapstoneProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CapstoneProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CapstoneProject, "CapstoneProject" );
+ 
---
+++ Source/CapstoneProject/CapstoneProjectGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CapstoneProjectGameMode.h"
+#include "CapstoneProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ACapstoneProjectGameMode::ACapstoneProjectGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
