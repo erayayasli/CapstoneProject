8031f6841f026312abc6bd48da8d6b344083072f
erayayasli <erayayasli623@gmail.com>
2025-03-01 18:01:31 +0300

    CPGameInstance Created. Created DataType folder for Our own structs and Enums.

---
+++ Source/CapstoneProject/CPGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CPGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONEPROJECT_API UCPGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
