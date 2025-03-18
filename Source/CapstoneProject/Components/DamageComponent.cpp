2cac081ca355bd44ee670f53c64ca87fe61c8f34
erayayasli <erayayasli623@gmail.com>
2025-03-18 14:46:15 +0300

    Created C++ version of DamageComponent, Will replace with BPC_DamageSystem.

---
+++ Source/CapstoneProject/Components/DamageComponent.cpp
@@ -0,0 +1,128 @@
+﻿
+#include "DamageComponent.h"
+#include "CapstoneProject/DataType/StructsClassesEnums.h"
+
+// Sets default values for this component's properties
+UDamageComponent::UDamageComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	Health = 100.f;
+	MaxHealth = 100.f;
+	bIsInvincible = false;
+	bIsDead = false;
+	bIsInterruptible = true;
+	bIsBlocking = false;
+	AttackTokensCount = 0;
+}
+
+
+// Called when the game starts
+void UDamageComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UDamageComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+
+float UDamageComponent::Heal(float Amount)
+{
+	if (!bIsDead)
+	{
+		Health = FMath::Clamp(Health + Amount, 0.f, MaxHealth);
+		return Health;
+	}
+	return -1;
+}
+
+bool UDamageComponent::TakeDamage(const FDamageInfo& DamageInfo, AActor* DamageCauser)
+{
+    bool BlockDamage, DoDamage, NoDamage;
+
+    // Get the output values ​​by calling the CanBeDamaged function
+    CanBeDamaged(DamageInfo.ShouldDamageInvincible, DamageInfo.CanBeBlocked, BlockDamage, DoDamage, NoDamage);
+
+    // If damage is blocked, trigger the OnBlocked event
+    if (BlockDamage)
+    {
+        OnBlocked.Broadcast(DamageInfo.CanBeParried, DamageCauser);
+        return false; // Hasar alınmadı
+    }
+
+    // If the damage is completely prevented (NoDamage), do nothing
+    if (NoDamage)
+    {
+        return false; // Hasar alınmadı
+    }
+
+    // If damage can be taken (DoDamage), reduce health value
+    if (DoDamage)
+    {
+        Health -= DamageInfo.Amount;
+
+        // Eğer sağlık sıfıra düştüyse, ölü duruma geç
+        if (Health <= 0.f)
+        {
+            bIsDead = true;
+            OnDeath.Broadcast();
+        }
+
+        // Trigger damage reaction event
+        OnDamageResponse.Broadcast(DamageInfo.DamageResponse);
+
+        return true; // Damage Taken
+    }
+
+    return false; // No damage taken by default
+}
+
+void UDamageComponent::ReserveAttackToken(int32 Amount)
+{
+}
+
+void UDamageComponent::ReturnAttackToken(int32 Amount)
+{
+}
+
+void UDamageComponent::CanBeDamaged(bool ShouldDamageInvincible, bool CanBeBlocked, bool& BlockDamage, bool& DoDamage, bool& NoDamage)
+{
+    // Set all outputs to false by default
+    BlockDamage = false;
+    DoDamage = false;
+    NoDamage = false;
+
+    if (bIsDead)
+    {
+        NoDamage = true; // Dead characters can not take damage
+        return;
+    }
+
+    if (!bIsInvincible || ShouldDamageInvincible)
+    {
+        if (!bIsBlocking || !CanBeBlocked)
+        {
+            DoDamage = true; // Can take damage
+            return;
+        }
+        else
+        {
+            BlockDamage = true; // Take damage is blocked
+            return;
+        }
+    }
+
+    NoDamage = true; // cannot take damage
+}
---
+++ Source/CapstoneProject/Components/DamageComponent.h
@@ -0,0 +1,86 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DamageComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CAPSTONEPROJECT_API UDamageComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDamageComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+    // Health Values
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+    float Health;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+    float MaxHealth;
+
+    // Situation Booleans
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+    bool bIsInvincible;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+    bool bIsDead;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+    bool bIsInterruptible;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+    bool bIsBlocking;
+
+    // Attack Tokens
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+    int32 AttackTokensCount;
+
+    // Damage Taking Functions
+    UFUNCTION(BlueprintCallable, Category = "Damage")
+    float Heal(float Amount);
+
+    UFUNCTION(BlueprintCallable, Category = "Damage")
+    bool TakeDamage(const FDamageInfo& DamageInfo, AActor* DamageCauser);
+
+    UFUNCTION(BlueprintCallable, Category = "Attack")
+    void ReserveAttackToken(int32 Amount);
+
+    UFUNCTION(BlueprintCallable, Category = "Attack")
+    void ReturnAttackToken(int32 Amount);
+
+
+    //******************************************************Dispatcherları ayarla**************************************************//
+    //Event Dispatcher Delegate Definitions
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnBlockedSignature, bool, CanBeParried, AActor*, DamageCauser);
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnDamageResponseSignature, EDamageResponse, DamageResponse);
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathSignature);
+
+    // Event Dispatchers
+    UPROPERTY(BlueprintAssignable, Category = "Events")
+    FOnBlockedSignature OnBlocked;
+
+    UPROPERTY(BlueprintAssignable, Category = "Events")
+    FOnDamageResponseSignature OnDamageResponse;
+
+    UPROPERTY(BlueprintAssignable, Category = "Events")
+    FOnDeathSignature OnDeath;
+
+    // Damageability Macro (BlueprintPure)
+    UFUNCTION(BlueprintPure, Category = "Damage")
+    void CanBeDamaged(bool ShouldDamageInvincible, bool CanBeBlocked, bool& BlockDamage, bool& DoDamage, bool& NoDamage);
+
+};
